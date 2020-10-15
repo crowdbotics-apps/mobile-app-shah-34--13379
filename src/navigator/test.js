8d530226296fcac880e481f431bb522fbccb117e
Shah RukhKhan <shah@crowdbotics.com>
2020-10-15 13:13:37 +0500

    Update screen

--- src/navigator/test.js
+++ src/navigator/test.js
@@ -28,7 +28,7 @@ export default class Blank extends React.Component {
 
   render = () => ( 
     <View style={styles.container}>
-      <Text>This is your asdfasdf</Text>
+      <Text>This is your </Text>
     </View>
   );
 }
